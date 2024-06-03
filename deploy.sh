51ad2024a71c29482cfc66fbe98d56c3e0a8148c
lib <13148121223@wo.cn>
2024-06-03 15:48:47 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 pnpm docs:build
 
 # 进入生成的文件夹
-cd dist
+cd src/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,7 +17,7 @@ git add -A
 
 git commit -m "deploy commit"
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master:gh-pages
+git push -f git@github.com:arclib/arclib.github.io.git master:gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
